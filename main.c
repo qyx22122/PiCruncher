d4a3f4c27fa712bbbeb806544d3d490ee98c239d
qyyx22122 <tibor.macek1@gmail.com>
2024-04-01 22:37:21 +0200

    Added a timer

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include "stdio.h"
 #include "gmp.h"
+#include "time.h"
 
-const int PREC = 10000;
+const int PREC = 40000;
 
 
 int main(int argc, char * argv[]){
+
+    time_t start , end;
 
     mpf_set_default_prec(PREC);
     
@@ -14,6 +17,8 @@ int main(int argc, char * argv[]){
     mpf_init(b);
     mpf_init(k);
     mpf_init(i);
+
+    time(&start);
     
 
     for(mpf_set_ui(k,0);mpf_cmp_ui(k,PREC/4) != 0; mpf_add_ui(k,k,1)){
@@ -70,6 +75,8 @@ int main(int argc, char * argv[]){
     FILE *pi_file = fopen("pi.txt", "w");
     int num_of_digits = mpf_out_str(pi_file,10,0,pi) - 4;
     printf("%d\n",num_of_digits);
+    time(&end);
+    printf("%.0lfs\n",(double)(end-start));
     return 0;
 
 }
